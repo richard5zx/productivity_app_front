9c6f4d27249557bd1d16c41c2df55663a5617a17
richard5zx <richardchao2009@gmail.com>
2024-09-10 14:28:15 +0800

    made template for webbrowser

---
+++ home.js
@@ -0,0 +1,7 @@
+var title="Welcome to Expense Tracker";
+$("#header").html(title);
+
+var navigation="<a href='index.html'>Homepage</a> | " +  
+                "<a href='login.html'>Login</a> | " +
+                "<a href='register.html'>Register</a>";
+$("#navigation").html(navigation);
