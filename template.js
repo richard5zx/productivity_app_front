48e848fcffe12b3e8d89bddfe7a900107753bbc0
richard5zx <richardchao2009@gmail.com>
2024-09-10 14:38:31 +0800

    changed file names

---
+++ template.js
@@ -0,0 +1,7 @@
+var title="Welcome to Expense Tracker";
+$("#header").html(title);
+
+var navigation="<a href='index.html'>Home</a> | " +  
+                "<a href='register.html'>Register</a> | " +
+                "<a href='login.html'>Login</a>";
+$("#navigation").html(navigation);
