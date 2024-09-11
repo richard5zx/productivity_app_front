c842b8c247acadb7201a7c6fc3fd132280d6d91f
richard5zx <richardchao2009@gmail.com>
2024-09-11 19:51:38 +0800

    edit format of the table for register

--- register.js
+++ register.js
@@ -1,12 +1,13 @@
-var prompt1="Please enter your details below to register<br>";
 var form="<form action='http://192.168.1.136:8080/register' method='post'>"+
-        "firstname=<input type='text' name='firstname'><br>"+
-        "lastname=<input type='text' name='lastname'><br>"+
-        "username=<input type='text' name='username'><br>"+
-        "password=<input type='password' name='password'><br>"+
-        "country=<input type='text' name='country'><br>"+
-        "email=<input type='text' name='email'><br>"+        
-        "<input type='submit' value='Register'>";
+        "<table width=60% align=center border=1>"+
+        "<tr><td colspan=2 align=center>Register details"+
+        "<tr><td width=20%>Firstname<td><input type='text' name='firstname'>"+
+        "<tr><td width=20%>Lastname<td><input type='text' name='lastname'><br>"+
+        "<tr><td width=20%>Username<td><input type='text' name='username'><br>"+
+        "<tr><td width=20%>Password<td><input type='password' name='password'><br>"+
+        "<tr><td width=20%>Country<td><input type='text' name='country'><br>"+
+        "<tr><td width=20%>Email<td><input type='text' name='email'><br>"+        
+        "<tr><td colspan=2 align='center'><input type='submit' value='Register'>";
 
-$("#content").html(prompt1+form);
+$("#content").html(form);
 
