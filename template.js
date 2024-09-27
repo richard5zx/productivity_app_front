5d709ac68dce04b0aee0f5aa6c3a8f701dda03e2
richard5zx <richardchao2009@gmail.com>
2024-09-27 19:07:16 +0800

    organize html in to folders

--- template.js
+++ template.js
@@ -1,7 +1,7 @@
 var title="Welcome to Expense Tracker";
 $("#header").html(title);
 
-var navigation="<a href='index.html'>Home</a> | " +  
-                "<a href='register.html'>Register</a> | " +
-                "<a href='login.html'>Login</a>";
+var navigation="<a href='/index.html'>Home</a> | " +  
+                "<a href='/register/register.html'>Register</a> | " +
+                "<a href='/login/login.html'>Login</a>";
 $("#navigation").html(navigation);
