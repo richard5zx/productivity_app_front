2c260dcc52d43ca7a981a6155a2c4073cfe53d94
richard5zx <richardchao2009@gmail.com>
2024-10-12 16:09:33 +0800

    merge logout function

--- expense/template_expense.js
+++ expense/template_expense.js
@@ -6,14 +6,8 @@ var navigation =
     "<a href='expense_browse.html'>Browse</a> | " +  
     "<a href='expense_add.html'>Add</a> | " +  
     "<a href='expense_update.html'>Update</a> | " +
-<<<<<<< HEAD
-    "<a href='expense_remove.html'>Remove</a> | " +
-    "<a href='/index.html'>Logout</a>";
-
-=======
     "<a href='expense_remove.html'>Remove</a> | "+
     "<a href='/index.html'>Logout</a>";
->>>>>>> 6616db2b078a7674c95bf0d1407b5ad939c36e60
 $("#navigation").html(navigation);
 
 var footer = "Expense Tracker";
